4723607048cfbaf48c3fa7475737065ccec1f321
chengyun <chengyun@ucap.com.cn>
2022-02-20 13:05:18 +0800

    第一次

---
+++ src/main/java/com/example/aquariusweb/AquariusWebApplication.java
@@ -0,0 +1,16 @@
+package com.example.aquariusweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ddd
+ */
+@SpringBootApplication
+public class AquariusWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquariusWebApplication.class, args);
+    }
+
+}
